97eb59c05524420d4360a235183f9136438bf286
DESKTOP-NHMLFB9\jdk <ehdrylang@naver.com>
2017-05-14 19:15:51 +0900

    listview html css 느낌 작성

--- board/views.py
+++ board/views.py
@@ -6,8 +6,15 @@ from board.models import Post
 class PostLV(ListView):
     model = Post
     template_name = 'board/post_all.html'
-    context_object_name = 'posts'
-    paginate_by = 12
+    context_object_name = 'posts' #Default : object_list
+    paginate_by = 2 #임시지정
+    queryset = Post.objects.all() #Default : Model.objects.all()
+
+    #count 구하는 함수 템플릿에서 {{count}}로 사용중
+    def get_context_data(self,**kwargs):
+        context = super(PostLV,self).get_context_data(**kwargs)
+        context['count'] = self.get_queryset().count()
+        return context
 
 class PostDV(DetailView):
     model = Post
